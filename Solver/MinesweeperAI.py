7c1a10e0fdb118f1861ed1aecccdeb35d7fd2813
Jaeyoon Lee <lee.jaeyoon.lee@gmail.com>
2022-12-08 02:04:57 -0500

    Update and add comments

--- Solver/MinesweeperAI.py
+++ Solver/MinesweeperAI.py
@@ -3,73 +3,90 @@
 
 import random
 
-
+# Constant: delta x&y around the position
 DXDY = ((1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,1),(-1,0),(-1,-1))
 
 class MinesweeperAI:
     def __init__(self, w:int, h:int, n_mines:int, start=0):
+        """w: width, h: height, n_mines: number of mines, start: start position"""
         self.w = w
         self.h = h
-        self.flags = []
-        self.proceesd = []
-        self.n_mines = n_mines
+        self.flags = [] # list of positions of flags
+        self.processed = [] # list of positions that already processed
+        self.n_mines = n_mines # number of mines
+        # start position (default: center of the board)
         self.start = start if isinstance(start, tuple) else (w//2, h//2)
-        self.tiles:dict = {self.start:0}
-        self.inputs = []
+        self.tiles:dict = {self.start:0} # Open tiles
 
+    # Add new open tiles
     def input(self, new_tiles:dict):
         for pos in new_tiles:
             if not pos in self.tiles.keys():
                 self.tiles[pos] = new_tiles[pos]
 
+    # main method
     def next_move(self):
-        to_open = []
-        flaged = []
-        for x,y in list(set(self.tiles)-set(self.proceesd)):
+        to_open = [] # tiles to open this turn
+        flaged = []  # tiles to put flags this turn
+        for x,y in list(set(self.tiles)-set(self.processed)): # for loop without processed tiles
             if self.tiles[(x,y)]>0:
-                count = 0
-                n_flag = 0
-                ds = []
+                ds = [] # list of nearby positions that aren't open without flags
+                n_flag = 0 # number of flags around the position
                 for dx,dy in DXDY:
-                    if self.is_in_board(x+dx, y+dy) and self.not_in_opens(x+dx, y+dy):
-                        count += 1
+                    if self.not_in_opens(x+dx, y+dy):
                         if self.not_in_flags(x+dx, y+dy):
                             ds.append((x+dx, y+dy))
                         else:
                             n_flag += 1
-                if count == self.tiles[(x,y)]:
+
+                if len(ds)+n_flag == self.tiles[(x,y)]:
                     for pos in ds:
+                        # if number of unopened tiles == tile number 
+                        # flag all unopened tiles around the position
                         self.flag(pos)
-                        flaged.append(pos)
-                    self.proceesd.append((x,y))
+                    # flaged += ds
+                    self.processed.append((x,y))
                 elif n_flag == self.tiles[(x,y)]:
-                    for dx,dy in DXDY:
-                        if (self.is_in_board(x+dx, y+dy) and self.not_in_opens(x+dx, y+dy)
-                            and self.not_in_flags(x+dx, y+dy)):
-                            to_open.append((x+dx,y+dy))
-                    self.proceesd.append((x,y))
+                    # if number of flags == tile number
+                    # Open all unopened tiles around the position
+                    to_open += ds
+                    self.processed.append((x,y))
+    
+        to_open = list(set(to_open))
+
         while len(to_open)==0:
+            # if cannot open anymore,
             newx,newy = random.randint(0,self.w-1), random.randint(0,self.h-1)
+            # pick a random location and open it
             if self.not_in_flags(newx,newy) and self.not_in_opens(newx,newy):
                 to_open.append((newx, newy))
+
         return to_open, flaged
 
+    # put a flag on the position
     def flag(self, pos:tuple[int,int]):
-        if not pos in self.flags:
+        if self.not_in_flags(pos[0],pos[1]):
             self.flags.append(pos)
 
+    # check win
     def is_win(self):
         return len(self.flags)==self.n_mines
 
+    # check position is in board
     def is_in_board(self, x, y):
         return (0 <= x < self.w) and (0 <= y < self.h)
 
+    # check position is not in an opens list
     def not_in_opens(self, x, y):
-        return not (x,y) in self.tiles.keys()
+        if self.is_in_board(x, y):
+            return not (x,y) in self.tiles.keys()
     
+    # check position is not in a flags list
     def not_in_flags(self, x, y):
-        return not(x, y) in self.flags
+        if self.is_in_board(x, y):
+            return not(x, y) in self.flags
 
+    # print the board on a terminal
     def show(self):
         for y in range(self.h):
             for x in range(self.w):
